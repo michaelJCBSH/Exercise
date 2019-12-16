740d8ae8cc498a3d63109a88493f92f04a796063
Michael ye <>
2019-12-16 11:21:17 +1100

    add collapsable actionbar on scroll feature

--- app/src/main/java/com/michael/presentation/AppActivity.kt
+++ app/src/main/java/com/michael/presentation/AppActivity.kt
@@ -1,11 +1,27 @@
 package com.michael.presentation
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.Navigation
 import com.michael.R
 
 class AppActivity : AppCompatActivity() {
+
+    var actionBarToolbar: Toolbar? = null
+        private set(value) {
+            field = value
+            if (value != null) {
+                setSupportActionBar(value)
+            }
+        }
+        get() {
+            if (field == null) {
+                field = findViewById(R.id.toolbar_actionbar)
+            }
+            return field
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +31,23 @@ class AppActivity : AppCompatActivity() {
         )
         val graph = navController.navInflater.inflate(R.navigation.app_nav_graph)
         navController.graph = graph
+    }
+
+    override fun setContentView(layoutResID: Int) {
+        actionBarToolbar = null
+        super.setContentView(layoutResID)
+        actionBarToolbar = findViewById(R.id.toolbar_actionbar)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (supportActionBar != null) {
+            val barHeight = resources.getDimensionPixelSize(R.dimen.abc_action_bar_default_height_material)
+
+            val actionBarToolbar = actionBarToolbar
+            actionBarToolbar?.layoutParams?.height = barHeight
+        }
     }
 
 }
